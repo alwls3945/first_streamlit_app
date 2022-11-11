0c19de3693b3d86e7dfca1b3d0da9b1a1ad7057c
Mijin Jung <117964366+alwls3945@users.noreply.github.com>
2022-11-11 13:36:29 +0900

    Update streamlit_app.py
    added a text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Dreakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocker Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
